ae5d22af6cd0d01e505c5c9f64cc364ab2cae339
mazylol <landonp95@gmail.com>
2022-03-19 17:17:52 -0400

    darker background

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   })
 
   return (
-    <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
+    <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-300">
       <div data-aos="fade-down" data-aos-duration="800">
         <Card />
       </div>
