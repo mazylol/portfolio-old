10686c0d719fe915ced8bce85dcc7b9ca27339f5
mazylol <landonp95@gmail.com>
2022-03-19 21:38:04 -0400

    better python icon

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,9 @@
 import React from "react"
-import { FaReact, FaPython } from "react-icons/fa"
+import { FaReact } from "react-icons/fa"
 import { BiGitBranch } from "react-icons/bi"
 import { SiJavascript, SiTailwindcss, SiLua, SiNetlify, SiC, SiRust, SiGnubash } from "react-icons/si"
 import { FcLinux } from "react-icons/fc"
+import { GiSandSnake } from "react-icons/gi"
  
 function Skills() {
   return (
@@ -24,7 +25,7 @@ function Skills() {
           <p className="mt-6 text-xl sm:text-2xl font-semibold text-center">Tailwind</p>
         </div>
         <div className="flex flex-col w-40 p-10 m-6 overflow-hidden bg-white shadow-2xl rounded-xl sm:w-52">
-          <FaPython color="#ffd343" className="mx-auto text-2xl sm:text-4xl" />
+          <GiSandSnake color="#3CCB91" className="mx-auto text-2xl sm:text-4xl" />
           <p className="mt-6 text-xl sm:text-2xl font-semibold text-center">Python</p>
         </div>
         <div className="flex flex-col w-40 p-10 m-6 overflow-hidden bg-white shadow-2xl rounded-xl sm:w-52">
